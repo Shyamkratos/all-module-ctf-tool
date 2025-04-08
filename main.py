f7d9bc511e7e1a20ce43785aef9e17d03b7ee73b
Shyamkratos <161335679+Shyamkratos@users.noreply.github.com>
2025-04-08 11:01:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,4 +29,7 @@ def index():
     return render_template_string(HTML, output=output, code=code)
 
 if __name__ == "__main__":
-    import os  port = int(os.environ.get("PORT", 5000)) app.run(host="0.0.0.0", port=port)
+   import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
